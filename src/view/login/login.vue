c66d0af78faee6f3ab1af4444227e8a15454e69d
466240326@qq.com <tizizhi123456~>
2020-05-06 16:26:37 +0800

    屏蔽登录

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -28,13 +28,14 @@ export default {
       'getUserInfo'
     ]),
     handleSubmit ({ userName, password }) {
-      this.handleLogin({ userName, password }).then(res => {
-        this.getUserInfo().then(res => {
-          this.$router.push({
-            name: this.$config.homeName
-          })
-        })
+      this.$router.push({
+        name: this.$config.homeName
       })
+      /*      this.handleLogin({ userName, password }).then(res => {
+              this.getUserInfo().then(res => {
+
+              })
+            }) */
     }
   }
 }
